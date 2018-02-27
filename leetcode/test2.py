8950277069cf90803f02b74a22168bc7229fb57a
liudandanddl <liudandanddl@didichuxing.com>
2018-02-27 11:48:56 +0800

    add

--- leetcode/test2.py
+++ leetcode/test2.py
@@ -6,18 +6,29 @@ import re
 __author__ = 'ldd'
 
 
-def majorityElement(nums):
+def trailingZeroes(n):
     """
-    :type nums: List[int]
+    求阶乘结果尾部0的个数.0是由2*5所得，计算能被多少个5，25，125.。。。。整除
+    :type n: int
     :rtype: int
     """
-    c = Counter(nums)
-    print(c)
-    return c.most_common(1)[0][0]
+    return 0 if n==0 else n/5 +trailingZeroes(n/5)
+
+
+def hammingWeight(n):
+    """
+    返回数字n变成二进制数后有多少个1
+    :type n: int
+    :rtype: int
+    """
+    str = bin(n)  # 0b1011  将整数转成二进制
+    str = str[2:]
+    return str.count('1')
+
+
 
 if __name__ == "__main__":
-    s = "  A man, a plan, a canal: Panama  "
-    print(majorityElement([1]))
-
+    print((10))  # 6个
+    # print('15511210043330985984000000'.count('0'))
 
 
