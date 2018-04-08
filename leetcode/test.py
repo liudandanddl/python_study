51d3c937b0799202a15969c8aa6bce817aea86b0
liudandanddl <liudandanddl@didichuxing.com>
2018-04-08 15:56:48 +0800

    add

--- leetcode/test.py
+++ leetcode/test.py
@@ -301,6 +301,27 @@ def rotate(nums, k):
     nums[:k], nums[k:] = nums[len(nums)-k:], nums[:len(nums)-k]
 
 
+def getSum(a, b):
+    """
+    位操作计算整数加法
+    :type a: int
+    :type b: int
+    :rtype: int
+    既然不能使用加法和减法，那么就用位操作。下面以计算5+4的例子说明如何用位操作实现加法：
+    1. 用二进制表示两个加数，a=5=0101，b=4=0100；
+    2. 用and（&）操作得到所有位上的进位carry=0100;
+    3. 用xor（^）操作找到a和b不同的位，赋值给a，a=0001；
+    4. 将进位carry左移一位，赋值给b，b=1000；
+    5. 循环直到进位carry为0，此时得到a=1001，即最后的sum。
+    因为Python的整数不是固定的32位，所以需要做一些特殊的处理。代码里的将一个数对0x100000000取模（注意：Python的取模运算结果恒为非负数），
+    是希望该数的二进制表示从第32位开始到更高的位都同是0（最低位是第0位），以在0-31位上模拟一个32位的int。
+    """
+    while b != 0:
+        carry = a & b
+        a = (a ^ b) % 0x100000000
+        b = (carry << 1) % 0x100000000
+    return a if a <= 0x7FFFFFFF else a | (~0x100000000+1)
+
 
 if __name__ == "__main__":
     # [4,-1,2,1] has the largest sum = 6.
