af6614319eea55e7bb62d6dc18ec5d964a99182a
liudandanddl <liudandanddl@didichuxing.com>
2018-05-18 10:27:00 +0800

    add

--- leetcode/test1.py
+++ leetcode/test1.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
+import sys
+
 __author__ = 'ldd'
 
 
@@ -31,4 +33,27 @@ def findAnagrams(s, p):
         i += 1
     if sdic == pdic:
         res.append(i)
+    return res
+
+
+def findMinArrowShots(points):
+    """
+    几箭可以射爆气球，输入的每个二维数组都是气球的宽度，若有一个气球的直径的开始和结束坐标为 xstart，xend，且满足xstart ≤ x ≤ xend，则该气球会被引爆。
+    输入:[[10,16], [2,8], [1,6], [7,12]]
+    输出:2
+    解释:对于该样例，我们可以在x = 6（射爆[2,8],[1,6]两个气球）和 x = 11（射爆另外两个气球）。
+    以第一个气球的终止位置为准，只要出现的气球起始位置小于这个气球的终止位置，代表可以一箭使这些气球全部爆炸；
+    当出现一个气球的起始位置大于第一个气球的终止位置时再以这个气球的终止位置为准，找出所有可以再一箭爆炸的所有气球；以此类推。。。
+    这道题目是活动选择问题(Activity-Selection Problem)的变形。活动选择问题是《算法导论》里面关于贪心算法的第一个问题。这个问题是这样的。
+    有一组活动，每个活动都有一个开始时间S和结束时间F，假设一个人在同一时间只能参加一个活动，找出出一个人可以参加的最多的活动数量。
+    :type points: List[List[int]]
+    :rtype: int
+    """
+    points.sort(key=lambda x:x[1])  # 二维数组，按照第二个元素排序
+    res = 0
+    last_end = -sys.maxint  # python的INT类型最大值
+    for p in points:
+        if p[0] > last_end:
+            last_end = p[1]
+            res += 1
     return res
--- leetcode/test2.py
+++ leetcode/test2.py
@@ -1,40 +1,113 @@
 #!/usr/bin/env python
 # coding=utf-8
 from collections import Counter
+import copy
+import sys
 
 __author__ = 'ldd'
 
 
-def findAnagrams(s, p):
-    """
-    :type s: str
-    :type p: str
-    :rtype: List[int]
-    Input:s: "cbaebabacd" p: "abc"  从s中查找有多少个p
-    Output: [0, 6]
-    Explanation:
-    The substring with start index = 0 is "cba", which is an anagram of "abc".
-    The substring with start index = 6 is "bac", which is an anagram of "abc".
-    """
-    res = [];  sdic = {};  pdic = {}
-    ls = len(s);  lp = len(p)
-    for i in s[:lp]:
-        sdic[i] = sdic.get(i, 0)+1
-    for i in p:
-        pdic[i] = pdic.get(i, 0)+1
-    i = 0
-    while i < ls-lp:
-        if sdic == pdic:
-            res.append(i)
-        sdic[s[i]] -= 1  # 删除s的第一个元素
-        if sdic[s[i]] == 0:
-            sdic.pop(s[i])  # 删除key
-        sdic[s[i+lp]] = sdic.get(s[i+lp], 0)+1
-        i += 1
-    if sdic == pdic:
-        res.append(i)
+def minWindow(s, t):
+    # res = ''  # 返回的最小窗口
+    # t_d = {}
+    # for i in t:
+    #     t_d[i] = t_d.get(i, 0)+1
+    # start = 0
+    # l_s = len(s)
+    # end = l_s-1
+    # s_d = copy.copy(t_d)
+    # flag = True
+    # while end < l_s and start < l_s:
+    #     if s[start] not in t:
+    #         start += 1
+    #         continue
+    #     if flag:
+    #         end = start
+    #         flag = False
+    #     if s[end] in s_d.keys():
+    #         s_d[s[end]] -= 1
+    #         if s_d[s[end]] == 0:
+    #             s_d.pop(s[end])
+    #     if not s_d:
+    #         res_now = s[start: end+1]
+    #         res = res_now if (len(res) >= len(res_now) or res == '') else res
+    #         start += 1
+    #         end = l_s-1
+    #         s_d = copy.copy(t_d)
+    #         flag = True
+    #         continue
+    #     end += 1
+    # return res
+
+    res = s
+    t_d = {}
+    flag = True
+    for i in range(len(s)):
+        if s[i] in t:
+            flag = False
+            start = i  # 初始位置
+            end = start
+            break
+    if flag:
+        return ''
+    for i in t:
+        t_d[i] = t_d.get(i, 0) + 1
+    s_d = copy.copy(t_d)
+    flag1 = False
+    while end < len(s) and start < len(s):
+        print(res, start, end)
+        if s[end] in s_d.keys():
+            if s_d[s[end]] == 1:
+                s_d.pop(s[end])
+            else:
+                s_d[s[end]] -= 1
+        if not s_d:
+            flag1 = True
+            res_now = s[start: end+1]
+            res = res_now if len(res) >= len(res_now) else res
+            s_d = copy.copy(t_d)
+        if flag1:
+            if s[end] != s[start]:
+                end += 1
+            else:
+                start += 1
+                if s[start] == s[end]:
+                    res_now = s[start: end+1]
+                    res = res_now if len(res) >= len(res_now) else res
+                    end += 1
+        else:
+            end += 1
     return res
+
+def maxProduct(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    max_ji = 1
+    now_ji = 1
+    n = 0
+    while n < len(nums):
+        if nums[n] > 0:
+            now_ji *= nums[n]
+            n += 1
+        else:
+            if n+1 < len(nums):
+                if nums[n+1] < 0:
+                    now_ji *= nums[n]*nums[n+1]
+                    n += 2
+                else:
+                    now_ji = nums[n+1]
+                    n += 2
+            else:
+                if now_ji:
+                    pass
+
+        max_ji = max_ji if max_ji > now_ji else now_ji
+    return max_ji
+
+
 
 
 if __name__ == "__main__":
-    print(findAnagrams(s='abaacbabc', p='abc'))
+    print(maxProduct([[2,3,-2,4]]))
